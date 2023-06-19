e6fc8669ccc56685b425b6302c778d7b7d84608d
Ishakkangah <ishakkangah@gmail.com>
2023-06-19 13:11:21 +0700

    Membuat API Register

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-import Login from './components/frontend/auth/Login';
-import Register from './components/frontend/auth/Register';
-import Home from './components/frontend/Home';
+import Login from "./components/frontend/auth/Login";
+import Register from "./components/frontend/auth/Register";
+import Home from "./components/frontend/Home";
 
-import MasterLayout from './layouts/admin/MasterLayout';
-import routes from './routes/routes';
+import MasterLayout from "./layouts/admin/MasterLayout";
+import routes from "./routes/routes";
+
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers["Accept"] = "application/json";
+axios.defaults.headers["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          
-          <Route path='/login' Component={Login}/>
-          <Route path='/register' Component={Register}/>
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
 
-
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/admin" element={<MasterLayout />}>
             {routes
-              .filter(route => route.component)
+              .filter((route) => route.component)
               .map(({ path, component: Component }) => (
-                <Route
-                  key={path}
-                  path={path}
-                  element={<Component />}
-                />
-              )
-            )}
-            <Route index element={<Navigate to="/admin/dashboard"/>} />
+                <Route key={path} path={path} element={<Component />} />
+              ))}
+            <Route index element={<Navigate to="/admin/dashboard" />} />
           </Route>
         </Routes>
       </Router>
--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,7 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import Navbar from "../../../layouts/frontend/Navbar";
 
 function Register() {
+  const [registerInput, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setRegister({ ...registerInput, [e.target.name]: e.target.value });
+  };
+
+  const registerSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      name: registerInput.name,
+      email: registerInput.email,
+      password: registerInput.password,
+    };
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      axios.post("/api/register", data).then((res) => {
+        if ((res.status = 200)) {
+          console.log("berhasil");
+        } else {
+          console.log(res.data.validation_errors);
+        }
+      });
+    });
+  };
+
   return (
     <div className="container">
       <Navbar />
@@ -12,64 +43,60 @@ function Register() {
               <div className="card-header">
                 <h2>Register</h2>
               </div>
-              <div className="card-body">
-                <div className="form-group mb-2">
-                  <label htmlFor="name" className="form-label require">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    name="id"
-                    placeholder="Masukan Nama"
-                  />
+              <form onChange={registerSubmit}>
+                <div className="card-body">
+                  <div className="form-group mb-2">
+                    <label htmlFor="name" className="form-label require">
+                      Fullname
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="name"
+                      name="name"
+                      placeholder="Masukan Nama"
+                      onChange={handleInput}
+                      value={registerInput.name}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="email" className="form-label require">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      placeholder="Masukan email"
+                      onChange={handleInput}
+                      value={registerInput.email}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <label htmlFor="password" className="form-label require">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      name="password"
+                      placeholder="********"
+                      onChange={handleInput}
+                      value={registerInput.password}
+                    />
+                  </div>
+                  <div className="form-group mb-2">
+                    <button
+                      type="submit"
+                      className="btn btn-primary w-100 shadow-sm"
+                    >
+                      Register
+                    </button>
+                  </div>
                 </div>
-                <div className="form-group mb-2">
-                  <label htmlFor="email" className="form-label require">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    name="email"
-                    placeholder="Masukan email"
-                  />
-                </div>
-                <div className="form-group mb-2">
-                  <label htmlFor="password" className="form-label require">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="password"
-                    placeholder="********"
-                  />
-                </div>
-                <div className="form-group mb-2">
-                  <label htmlFor="password" className="form-label require">
-                    Confirm Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="confirm_password"
-                    placeholder="********"
-                  />
-                </div>
-                <div className="form-group mb-2">
-                  <button
-                    type="submit"
-                    className="btn btn-primary w-100 shadow-sm"
-                  >
-                    Register
-                  </button>
-                </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
